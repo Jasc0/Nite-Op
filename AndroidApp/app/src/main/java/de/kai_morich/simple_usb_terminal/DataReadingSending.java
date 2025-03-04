092fa1dfeb75922d72f1a8e29ffd190ee657e8b7
JessTells <jernestot27@gmail.com>
2025-03-04 15:54:47 -0800

    DataReadingSending getting DB client

--- AndroidApp/app/src/main/java/de/kai_morich/simple_usb_terminal/DataReadingSending.java
+++ AndroidApp/app/src/main/java/de/kai_morich/simple_usb_terminal/DataReadingSending.java
@@ -3,7 +3,11 @@ package de.kai_morich.simple_usb_terminal;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import com.mongodb.*;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
 public class DataReadingSending {
     private ArrayList<byte[]> receivedBytes;
@@ -13,7 +17,15 @@ public class DataReadingSending {
     DataReadingSending(TextView debug){
         receivedBytes = new ArrayList<>();
         debug_text = debug;
-        mongoClient = new MongoClient();
+        try {
+            File myObj = new File("z_secrets.txt");
+            Scanner myReader = new Scanner(myObj);
+            String serverString = myReader.nextLine();
+            myReader.close();
+            mongoClient = new MongoClient(serverString);
+        } catch (FileNotFoundException e) {
+            debug_text.setText("No DB string");
+        }
     }
 
     public void receiveData(byte[] data){
